89c8dcbee927e5e6b06010e227f6ae87d6770f89
laurissalcedo37 <laurissalcedo@byui.edu>
2021-10-21 16:42:43 -0500

    lesson 6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,9 @@
-var temp = document.querySelector("#temp").innerHTML;
 var windspeed = document.querySelector("#windspeed").innerHTML;
+var tempeture = document.querySelector("#tempeture").innerHTML;
 
-var f = Math.round(35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16)));
+var f = Math.round(35.74 + (0.6215 * tempeture) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * tempeture * Math.pow(windspeed, 0.16)));
 
-if (temp > 50 && windspeed > 3.0) {
+if (tempeture > 50 && windspeed > 3.0) {
     document.querySelector("#windchill").innerHTML = "N/A";
 } else {
     document.querySelector("#windchill").innerHTML = f;
